2d9b1e8cfc1d0e78522aa47e508356614f79d1d8
Rompeled8 <rompeled8@gmail.com>
2024-11-27 21:02:39 +0200

    Add files via upload

--- ex1.c
+++ ex1.c
@@ -1,43 +1,59 @@
 /******************
-Name:
-ID:
-Assignment:
+Name:Rom Peled
+Assignment:ex1
 *******************/
 #include <stdio.h>
 
-// REMIDER : YOU CANT USE ANY CONTROL FLOW OPERATIONS OR FUNCTIONS, ONLY BITWISE.
-
 int main() {
-  
-  // What bit
   printf("What bit:\n");
-  /*Scan two integers (representing number and a position)
-  Print the bit in this position. */
-  
-  // Set bit
-  printf("\nSet bit:\n");
-  /*Scan two integers (representing number and a position)
-  Make sure the bit in this position is "on" (equal to 1)
-  Print the output
-  Now make sure it's "off" (equal to 0)
-  Print the output */
+  int number, position;
+  printf("Please enter a number:\n");
+  scanf("%d", &number);
+  printf("Please enter a position:\n");
+  scanf("%d",&position);
+  int bit = (number>>position) & 1;     //Moves the wanted bit to LSB spot and extract the value//
+  printf("The bit in position %d of number %d is: %d\n", position,number, bit);
 
-  // Toggle bit
-  printf("\nToggle bit:\n");
-  /*Scan two integers (representing number and a position)
-  Toggle the bit in this position
-  Print the new number */
-  
-  // Even - Odd
-  printf("\nEven - Odd:\n");
-  /* Scan an integer
-  If the number is even - print 1, else - print 0. */
-  
-  // 3, 5, 7, 11
-  printf("\n3, 5, 7, 11:\n");
-  /* Scan two integers in octal base
-  sum them up and print the result in hexadecimal base
-  Print only 4 bits, in positions: 3,5,7,11 in the result. */
+  printf("Set bit:\n");
+  int num, pos;
+  printf("Please enter a number:\n");
+  scanf("%d", &num);
+  printf("Please enter a position:\n");
+  scanf("%d",&pos);
+  int bit1 = num | (1<<pos); //Sets the wanted bit to 1
+  int bit0 = num & ~(1<<pos); //Sets the wanted bit to 0
+  printf("Number with bit %d set to 1: %d\n", pos,bit1);
+  printf("Number with bit %d set to 0: %d\n",pos,bit0);
+
+  printf("Toggle bit:\n");
+  int Number, Position;
+  printf("Please enter a number:\n");
+  scanf("%d", &Number);
+  printf("Please enter a position:\n");
+  scanf("%d",&Position);
+  int toggled = Number ^ (1<<Position); //Toggles the wanted bit
+  printf("Number with bit %d toggled: %d\n",Position,toggled);
+
+  printf("Even - Odd:\n");
+  int Num;
+  printf("Please enter a number:\n");
+  scanf("%d", &Num);
+  int check = (Num & 1) ^ 1; //Check the LSB to see if even or odd and sets even as 1 odd as 0
+  printf("%d\n",check);
+
+  printf("3, 5, 7, 11:\n");
+  unsigned int num1,num2,sum,bit3,bit5,bit7,bit11;
+  printf("Please enter the first number (octal):\n");
+  scanf("%o",&num1);
+  printf("Please enter the second number (octal):\n");
+  scanf("%o",&num2);
+  sum = num1 + num2;
+  printf("The sum in hexadecimal: %X\n", sum);
+  bit3 = (sum>>3) &1; //Gets to the wanted bit and collects its value
+  bit5 = (sum>>5) &1;
+  bit7 = (sum>>7) &1;
+  bit11 = (sum>>11) &1;
+  printf("The 3,5,7,11 bits are: %u%u%u%u\n",bit3,bit5,bit7,bit11);
 
   printf("Bye!\n");
   
